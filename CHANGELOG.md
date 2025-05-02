31228f1380e1083135cc0b957ebc4ad29c545d0e
JavaCommons Technologies <javacommons@gmail.com>
2025-05-02 19:47:16 +0900

    debug_output: Initial release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,12 @@
 # CHANGELOG.md
+
+## 2025.502.1946
+
+- Initial release
+
+```
+--- a/pubspec.yaml
++++ b/pubspec.yaml
+-version: 0.0.1
++version: 2025.502.1946
+```
--- lib/debug_output.dart
+++ lib/debug_output.dart
@@ -15,7 +15,7 @@ List<String> _textToLines(String s) {
 }
 
 String echo(dynamic x, {String? title, bool silent = false, String? type}) {
-  String json = switch(type) {
+  String json = switch (type) {
     'json' => text_serializer.toJson(x, '  '),
     'yaml' => '[YAML]```\n${text_serializer.toYaml(x)}```[/YAML]',
     _ => (x is String) ? '`$x`' : '$x',
@@ -33,7 +33,7 @@ String echo(dynamic x, {String? title, bool silent = false, String? type}) {
 }
 
 String dump(dynamic x, {String? title, bool silent = false, String? type}) {
-  String json = switch(type) {
+  String json = switch (type) {
     'json' => text_serializer.toJson(x, '  '),
     'yaml' => '[YAML]```\n${text_serializer.toYaml(x)}```[/YAML]',
     _ => (x is String) ? '`$x`' : '$x',
